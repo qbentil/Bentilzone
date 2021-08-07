a3602b0586fe2fa7e18148a178cd439bf844ccf5
qbentil <Bentilshadrack72@gmail.com>
2021-08-07 13:06:47 +0000

    Adding HttpCleint module

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Frontend';
+  title = 'Bentilzone Eccomerce';
   name = 'Bentil Shadrack';
 }
