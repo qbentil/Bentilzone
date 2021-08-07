1fc54267dbb44b9095f0d4114175cf3b6a8858aa
qbentil <Bentilshadrack72@gmail.com>
2021-08-07 01:21:34 +0000

    Frontend: Prodject Struturing / Routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { ProductComponent } from './components/product/product.component';
+import { ThankyouComponent } from './components/thankyou/thankyou.component';
+
+const routes: Routes = [
+  {
+   path: '', component: HomeComponent 
+  },
+  {
+    path: 'product/:id', component:ProductComponent
+  },
+  {
+    path: 'cart', component:CartComponent
+  },
+  {
+    path: 'checkout', component:CheckoutComponent
+  },
+  {
+    path: 'thankyou', component:ThankyouComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CartComponent } from './components/cart/cart.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { ThankyouComponent } from './components/thankyou/thankyou.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartComponent,
+    CheckoutComponent,
+    HomeComponent,
+    ProductComponent,
+    ThankyouComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
