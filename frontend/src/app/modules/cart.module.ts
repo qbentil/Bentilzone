eaa882da2271cd7bbb76949bb69fba082c950fab
qbentil <Bentilshadrack72@gmail.com>
2021-08-08 21:39:53 +0000

    Frontend: Creatring Cart Module

---
+++ frontend/src/app/modules/cart.module.ts
@@ -0,0 +1,16 @@
+import { ProductModuleServer } from './product.module';
+export interface CartModuleServer {
+  totalAmount: number;
+  data: [{
+    product: ProductModuleServer,
+    numInCart: number
+  }];
+}
+
+export interface CartModulePublic{
+  total: number;
+  prodData: [{
+    id: number,
+    incart: number
+  }];
+}
