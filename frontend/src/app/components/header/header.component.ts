6bbc473cf9cc01fc792ea82817c48a153e885745
qbentil <Bentilshadrack72@gmail.com>
2021-08-17 04:50:51 +0000

    Display Cart Count and Product in Navbar

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartModuleServer } from './../../modules/cart.module';
+import { CartService } from './../../services/cart.service';
+import { ProductModuleServer } from 'src/app/modules/product.module';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    // Create an undefined products
+  private pro: ProductModuleServer = {
+    id: 0,
+    name: '',
+    price: 0,
+    quantity: 0,
+    description: '',
+    images: '',
+    image: ''
+  };
+
+  cartData : CartModuleServer = {
+    totalAmount: 0,
+    data: [{
+      product: this.pro,
+      numInCart: 0,
+    }]
+  };
+  cartTotal: number = 0;
+
+
+
+  constructor(public cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartService.cartTotal$.subscribe(total => this.cartTotal = total)
+    this.cartService.cartData$.subscribe(data => this.cartData = data)
   }
 
 }
