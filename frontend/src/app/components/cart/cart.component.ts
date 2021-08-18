1af802922af366379e48ec2321f9f55698b08917
qbentil <Bentilshadrack72@gmail.com>
2021-08-18 00:39:42 +0000

    Finishing Cart Component and page🎉

--- frontend/src/app/components/cart/cart.component.ts
+++ frontend/src/app/components/cart/cart.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartModuleServer } from './../../modules/cart.module';
+import { CartService } from './../../services/cart.service';
+import { ProductModuleServer } from 'src/app/modules/product.module';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+      // Create an undefined products
+      private pro: ProductModuleServer = {
+        id: 0,
+        name: '',
+        price: 0,
+        quantity: 0,
+        description: '',
+        images: '',
+        image: ''
+      };
+
+      cartData : CartModuleServer = {
+        totalAmount: 0,
+        data: [{
+          product: this.pro,
+          numInCart: 0,
+        }]
+      };
+      cartTotal: number = 0;
+      subTotal: number = 0;
+
+
+  constructor(public cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartService.cartData$.subscribe((data:CartModuleServer) => this.cartData = data)
+    this.cartService.cartTotal$.subscribe((total:number) => this.cartTotal = total )
+  }
+  public changeQuantity(i: number, inc: boolean)
+  {
+    this.cartService.updateCartItems(i, inc)
   }
 
 }
