e851757e5058c46e5415a768bf75164df4459216
qbentil <Bentilshadrack72@gmail.com>
2021-08-23 11:24:01 +0000

    Debugging 1.01

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -192,8 +192,7 @@ router.post("/new", (req, res) =>{
 
 /* FAKE PAYMENT GATEWAY CALL */
 router.post('/payment', (req, res) => {
-  includes(res) //{Uses Request}
-  // includes(req)
+  includes(res);
   setTimeout( () => {
     res.status(200).json({success: true});
   }, 3000)
@@ -205,11 +204,11 @@ router.post('/payment', (req, res) => {
 
 function includes(res)
 {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200")
+  res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Expose-Headers", " Content-Length, X-JSON");
   res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Preflight-Continue", true); //Preflight-Continue
+  res.setHeader("Access-Control-Preflight-Continue", false); //Preflight-Continue
   res.setHeader("Access-Control-Allow-Headers", "content-type, Authorization, Origin, X-Requested-with, Accept");
   res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );  
 }
